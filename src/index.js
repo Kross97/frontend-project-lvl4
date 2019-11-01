8d462d6e7745b3895e05e88f228a63213702b200
Nikita Bezhenar <kross78080@yandex.ru>
2019-11-01 17:50:58 +0200

    2

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/application.css';
+
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.debug = 'chat:*';
+}
+
+document.body.textContent = '<b>mffffffffffake</b> love';
